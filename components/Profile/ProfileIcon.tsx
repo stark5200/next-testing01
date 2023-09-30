431bd77fe3edd5b14db84b3337d64eb23d538259
stark5200 <a.taha5200@gmail.com>
2023-10-01 01:43:36 +0200

    some basic layout

---
+++ components/Profile/ProfileIcon.tsx
@@ -0,0 +1,11 @@
+import { DEFAULT_PROFILE_ICON_SIZE } from "constants/Profile";
+
+export default function ProfileIcon() {
+  return (
+    <div style={{
+      width: DEFAULT_PROFILE_ICON_SIZE
+    }}>
+      <p>Icon</p>
+    </div>
+  )
+}
